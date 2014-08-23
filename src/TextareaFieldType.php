a1d2e7fccfedad153690f0de92ecf0a7ff890b0b
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 20:15:43 -0500

    Initial commit.

---
+++ src/TextareaFieldType.php
@@ -0,0 +1,40 @@
+<?php namespace Addon\FieldType\Textarea;
+
+use Streams\Core\Addon\FieldTypeAbstract;
+
+class TextareaFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'string';
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/',
+    );
+
+    /**
+     * Return the input used for forms.
+     *
+     * @return mixed
+     */
+    public function formInput()
+    {
+        return \Form::textarea($this->formSlug, $this->value);
+    }
+}
