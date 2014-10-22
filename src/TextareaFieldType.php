62a27cf03e0fc48841a7ae32544c1966d79e4bb5
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:47:25 -0500

    Clean up before final

--- src/TextareaFieldType.php
+++ src/TextareaFieldType.php
@@ -4,20 +4,8 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class TextareaFieldType extends FieldTypeAddon
 {
-    protected $slug = 'textarea';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
     public $columnType = 'text';
 
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
     public function input()
     {
         return \Form::textarea($this->inputName(), $this->value);
