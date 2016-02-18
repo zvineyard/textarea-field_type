5bbe3e6439070c1bc93630fc798a430e148dfaa2
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 01:12:22 -0600

    Author

--- src/TextareaFieldTypePresenter.php
+++ src/TextareaFieldTypePresenter.php
@@ -6,9 +6,9 @@ use Symfony\Component\Yaml\Yaml;
 /**
  * Class TextareaFieldTypePresenter
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\TextareaFieldType
  */
 class TextareaFieldTypePresenter extends FieldTypePresenter
