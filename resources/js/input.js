f91f2c2d9984d1a510e12667f5c2ac139cab7923
Ryan Thompson <ryan@pyrocms.com>
2017-09-06 20:18:40 -0500

    Remove dat

--- resources/js/input.js
+++ resources/js/input.js
@@ -18,39 +18,5 @@ $(document).on('ajaxComplete ready', function () {
                 counter.removeClass('text-danger');
             }
         });
-
-        /*$(this).textcomplete([
-         {
-         tags: ['{{ entry.last_name }}', '{{ forms_display($slug) }}'],
-         match: /{{2}\s[a-z_.]+(?!}})$/,
-         search: function (term, callback) {
-         callback($.map(this.tags, function (tag) {
-         return tag.indexOf(term) === 0 ? tag : null;
-         }));
-         },
-         index: 0,
-         replace: function (tag) {
-         return tag;
-         }
-         },
-         {
-         tags: [
-         '{input.subject}',
-         '{input.email}',
-         '{input.phone}',
-         '{input.name}'
-         ],
-         match: /{{1}[a-z_.]+(?!}})$/,
-         search: function (term, callback) {
-         callback($.map(this.tags, function (tag) {
-         return tag.indexOf(term) === 0 ? tag : null;
-         }));
-         },
-         index: 0,
-         replace: function (tag) {
-         return tag;
-         }
-         }
-         ]);*/
     });
 });
