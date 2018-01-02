584598cf52028bf953cdfb230512b36c8ee63a52
Denis Efremov <efremov.a.denis@gmail.com>
2018-01-03 02:14:05 +0300

    Update input.js

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,44 +1,49 @@
 (function (window, document) {
 
-    let fields = Array.from(
-        document.querySelectorAll('textarea[data-provides="anomaly.field_type.textarea"]')
-    );
+    const init = function () {
 
-    fields.forEach(function (field) {
+        const textareas = document.querySelectorAll('textarea[data-provides="anomaly.field_type.textarea"]');
+        const fields = textareas.length > 0
+            ? Array.from(textareas)
+            : [];
 
-        let wrapper = field.closest('div');
-        let counter = wrapper.querySelector('.counter');
-        let count = counter.querySelector('.count');
+        fields.forEach(function (field) {
 
-        let max = field.dataset.max;
+            const wrapper = field.closest('div');
+            const counter = wrapper.querySelector('.counter');
+            const count = counter.querySelector('.count');
+            const max = field.dataset.max;
 
-        /**
-         * Listen for keyup and update
-         * the counter and contexts.
-         */
-        field.addEventListener('keyup', function () {
+            /**
+             * Listen for keyup and update
+             * the counter and contexts.
+             */
+            field.addEventListener('keyup', function () {
 
-            if (max) {
-                count.innerText = max - field.value.length;
-            } else {
-                count.innerText = field.value.length;
-            }
+                if (max) {
+                    count.innerText = max - field.value.length;
+                } else {
+                    count.innerText = field.value.length;
+                }
 
-            if (max && field.value.length > max) {
-                counter.classList.add('text-danger');
-            } else {
-                counter.classList.remove('text-danger');
-            }
+                if (max && field.value.length > max) {
+                    counter.classList.add('text-danger');
+                } else {
+                    counter.classList.remove('text-danger');
+                }
+            });
+
+            /**
+             * Fire the count event initially
+             * to cause an initial count.
+             */
+            const event = document.createEvent('HTMLEvents');
+
+            event.initEvent('keyup', false, true);
+            field.dispatchEvent(event);
         });
+    };
 
-        /**
-         * Fire the count event initially
-         * to cause an initial count.
-         */
-        let event = document.createEvent('HTMLEvents');
-
-        event.initEvent('keyup', false, true);
-
-        field.dispatchEvent(event);
-    });
+    
+    document.addEventListener('DOMContentLoaded', init);
 })(window, document);
