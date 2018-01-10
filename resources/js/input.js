189e09b25d01a121dce20c73b6ad429f30c4f41c
Ryan Thompson <ryan@pyrocms.com>
2018-01-10 07:26:06 -0600

    Clean up JS / fix issue with grid.

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,54 +1,49 @@
 (function (window, document) {
 
-    const init = function () {
+    const fields = Array.from(
+        document.querySelectorAll('textarea[data-provides="anomaly.field_type.textarea"]')
+    );
 
-        const textareas = Array.from(
-            document.querySelectorAll('textarea[data-provides="anomaly.field_type.textarea"]')
-        );
-        
-        fields.forEach(function (field) {
+    fields.forEach(function (field) {
 
-            const wrapper = field.parentElement;
-            const max = field.dataset.max;
+        const wrapper = field.parentElement;
+        const max = field.dataset.max;
 
-            /**
-             * Listen for keyup and update
-             * the counter and contexts.
-             */
-            field.addEventListener('keyup', function () {
+        /**
+         * Listen for keyup and update
+         * the counter and contexts.
+         */
+        field.addEventListener('keyup', function () {
 
-                const counter = wrapper.querySelector('.counter');
-                const count = wrapper.querySelector('.count');
+            const counter = wrapper.querySelector('.counter');
+            const count = wrapper.querySelector('.count');
 
-                if (count) {
-                  if (max) {
-                      count.innerText = max - field.value.length;
-                  } else {
-                      count.innerText = field.value.length;
-                  }
+            if (count) {
+                if (max) {
+                    count.innerText = max - field.value.length;
+                } else {
+                    count.innerText = field.value.length;
                 }
+            }
 
-                if (counter) {
-                  if (max && field.value.length > max) {
-                      counter.classList.add('text-danger');
-                  } else {
-                      counter.classList.remove('text-danger');
-                  }
+            if (counter) {
+                if (max && field.value.length > max) {
+                    counter.classList.add('text-danger');
+                } else {
+                    counter.classList.remove('text-danger');
                 }
-            });
-
-            /**
-             * Fire the count event initially
-             * to cause an initial count.
-             */
-            const event = document.createEvent('HTMLEvents');
-
-            event.initEvent('keyup', false, true);
-            
-            field.dispatchEvent(event);
+            }
         });
-    };
 
-    document.addEventListener('DOMContentLoaded', init);
+        /**
+         * Fire the count event initially
+         * to cause an initial count.
+         */
+        const event = document.createEvent('HTMLEvents');
+
+        event.initEvent('keyup', false, true);
+
+        field.dispatchEvent(event);
+    });
     
 })(window, document);
